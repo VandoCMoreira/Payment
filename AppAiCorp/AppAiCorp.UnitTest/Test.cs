4f427027a4f5bda76b999ba564b297a8df56b7c7
VandoCMoreira <fvand@hotmail.com>
2018-12-03 23:08:57 +0000

    adding tests

--- AppAiCorp/AppAiCorp.UnitTest/Test.cs
+++ AppAiCorp/AppAiCorp.UnitTest/Test.cs
@@ -13,10 +13,11 @@ namespace AppAiCorp.UnitTest
     {
 
         [Test()]
-        public void Crate_Payment_ok()
+        public void Crate_Payment_Success()
         {
             var MerchantId = "Progra-7702818";
             var http = HttpContext.Current;
+
             try
             {
                 Payment SubmitProcessPayment = new Payment(MerchantId, http);
@@ -30,7 +31,7 @@ namespace AppAiCorp.UnitTest
         }
 
         [Test()]
-        public void Crate_Payment_Merch()
+        public void Crate_Payment_MerchantId_Empty()
         {
             var MerchantId = "";
             var http = HttpContext.Current;
@@ -46,12 +47,11 @@ namespace AppAiCorp.UnitTest
         }
 
         [Test()]
-        public void Crate_Payment_http()
+        public void Crate_Payment_Http_Empty()
         {
             var MerchantId = "Progra-7702818";
-            var http = HttpContext.Current;
-            http = null;
-
+            HttpContext http = null;
+        
             try
             {
                 Payment SubmitProcessPayment = new Payment(MerchantId, http);
@@ -63,11 +63,10 @@ namespace AppAiCorp.UnitTest
         }
 
         [Test()]
-        public void Crate_Payment_both()
+        public void Crate_Payment_All_Empty()
         {
             var MerchantId = "";
-            var http = HttpContext.Current;
-            http = null;
+            HttpContext http = null;
 
             try
             {
@@ -77,18 +76,19 @@ namespace AppAiCorp.UnitTest
             {
                 Assert.IsTrue(ex is System.ArgumentNullException);
             }
+
         }
 
         [Test()]
-        public void Crate_Remote_Http()
+        public void Create_Remote_PostUrl_Empty()
         {
             var postUrl = "";
-            var _context = HttpContext.Current;
+            var context = HttpContext.Current;
             var Default = DefaultMethod.Post;
         
             try
             {
-                Remote remote = new Remote(_context, postUrl, Default);
+                Remote remote = new Remote(context, postUrl, Default);
             }
             catch (System.Exception ex)
             {
@@ -97,16 +97,15 @@ namespace AppAiCorp.UnitTest
         }
 
         [Test()]
-        public void Crate_Remote_Context()
+        public void Crate_Remote_Context_Empty()
         {
             var postUrl = "https://mms.payvector.net/Pages/PublicPages/PaymentForm.aspx";
-            var _context = HttpContext.Current;
-            _context = null;
+            var context = HttpContext.Current = null;
             var Default = DefaultMethod.Post;
 
             try
             {
-                Remote remote = new Remote(_context, postUrl, Default);
+                Remote remote = new Remote(context, postUrl, Default);
             }
             catch (System.Exception ex)
             {
@@ -115,45 +114,45 @@ namespace AppAiCorp.UnitTest
         }
 
         [Test()]
-        public void Crate_Remote_Both()
+        public void Crate_Remote_Context_Default_Empty()
         {
             var postUrl = "";
-            var _context = HttpContext.Current;
-            _context = null;
+            var context = HttpContext.Current = null;
             var Default = DefaultMethod.Post;
 
             try
             {
-                Remote remote = new Remote(_context, postUrl, Default);
+                Remote remote = new Remote(context, postUrl, Default);
             }
             catch (System.Exception ex)
             {
                 Assert.IsTrue(ex is System.ArgumentNullException);
             }
+
         }
 
         [Test()]
-        public void post_full()
+        public void Post_Sucess()
         {
             var formName = "CardsavePaymentForm";
             var postUrl = "https://mms.payvector.net/Pages/PublicPages/PaymentForm.aspx";
-            var _context = HttpContext.Current;
+            var context = HttpContext.Current;
             var Default = DefaultMethod.Post;
 
-           
-             try
+            try
             {
-                Remote remote = new Remote(_context, postUrl, Default);
+                Remote remote = new Remote(context, postUrl, Default);
                 remote.Post(formName);
             }
             catch (System.Exception ex)
             {
                 Assert.IsTrue(ex is System.NullReferenceException);
             }
+
         }
 
         [Test()]
-        public void post_formName()
+        public void Post_FormName_Empty()
         {
             var formName = "";
             var postUrl = "https://mms.payvector.net/Pages/PublicPages/PaymentForm.aspx";
@@ -173,7 +172,67 @@ namespace AppAiCorp.UnitTest
         }
 
         [Test()]
-        public void Query_String()
+        public void Post_PostUrl_Empty()
+        {
+            var formName = "CardsavePaymentForm";
+            var postUrl = "";
+            var context = HttpContext.Current;
+            var Default = DefaultMethod.Post;
+
+
+            try
+            {
+                Remote remote = new Remote(context, postUrl, Default);
+                remote.Post(formName);
+            }
+            catch (System.Exception ex)
+            {
+                Assert.IsTrue(ex is System.ArgumentNullException);
+            }
+        }
+
+        [Test()]
+        public void Post_Context_Empty()
+        {
+            var formName = "CardsavePaymentForm";
+            var postUrl = "https://mms.payvector.net/Pages/PublicPages/PaymentForm.aspx";
+            var context = HttpContext.Current = null;
+            var Default = DefaultMethod.Post;
+
+
+            try
+            {
+                Remote remote = new Remote(context, postUrl, Default);
+                remote.Post(formName);
+            }
+            catch (System.Exception ex)
+            {
+                Assert.IsTrue(ex is System.NullReferenceException);
+            }
+        }
+
+        [Test()]
+        public void Post_All_Empty()
+        {
+            var formName = "CardsavePaymentForm";
+            var postUrl = "https://mms.payvector.net/Pages/PublicPages/PaymentForm.aspx";
+            var context = HttpContext.Current = null;
+            var Default = DefaultMethod.Post;
+
+
+            try
+            {
+                Remote remote = new Remote(context, postUrl, Default);
+                remote.Post(formName);
+            }
+            catch (System.Exception ex)
+            {
+                Assert.IsTrue(ex is System.NullReferenceException);
+            }
+        }
+
+        [Test()]
+        public void Query_String_Success()
         {
             Transaction request = new Transaction();
             var nvCollection = new NameValueCollection();
@@ -190,7 +249,7 @@ namespace AppAiCorp.UnitTest
         }
 
         [Test()]
-        public void Query_String_True()
+        public void Query_String_True_True()
         {
             Transaction request = new Transaction();
             var nvCollection = new NameValueCollection();
@@ -198,6 +257,39 @@ namespace AppAiCorp.UnitTest
             try
             {
                 nvCollection.ToQueryString(true, true);
+
+            }
+            catch (System.Exception ex)
+            {
+                Assert.IsTrue(ex is System.NullReferenceException);
+            }
+        }
+
+        [Test()]
+        public void Query_String_False_True()
+        {
+            Transaction request = new Transaction();
+            var nvCollection = new NameValueCollection();
+
+            try
+            {
+                nvCollection.ToQueryString(false, true);
+
+            }
+            catch (System.Exception ex)
+            {
+                Assert.IsTrue(ex is System.NullReferenceException);
+            }
+        }
+        [Test()]
+        public void Query_String_True_False()
+        {
+            Transaction request = new Transaction();
+            var nvCollection = new NameValueCollection();
+
+            try
+            {
+                nvCollection.ToQueryString(true, false);
 
             }
             catch (System.Exception ex)
